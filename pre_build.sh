c15bebfd05f2ca35344f2de39a0ce2eb3c6325a3
Tarak Ben Youssef <tarak.benyoussef@dapperlabs.com>
2024-02-20 19:34:40 -0600

    move build flags to shell script

--- pre_build.sh
+++ pre_build.sh
@@ -5,9 +5,11 @@ set -e
 
 # INPUT_GOARCH is a variable set by the wangyoucao577 action
 # https://github.com/wangyoucao577/go-release-action/blob/v1.40/action.yml#L109
-C_COMP=""
 if [ "${INPUT_GOARCH}" = "arm64" ]; then
-    C_COMP="aarch64-linux-gnu-gcc"
+    CC="aarch64-linux-gnu-gcc"
 elif [ "${INPUT_GOARCH}" = "amd64" ]; then
-    C_COMP="x86_64-linux-gnu-gcc"
+    CC="x86_64-linux-gnu-gcc"
 fi
+
+CGO_ENABLED=1
+CGO_FLAGS=\""-O2 -D__BLST_PORTABLE__"\"
