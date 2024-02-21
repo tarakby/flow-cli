c2cf8c901acadb15c92c3c8c0b8942f24abbdd35
Tarak Ben Youssef <tarak.benyoussef@dapperlabs.com>
2024-02-20 19:27:12 -0600

    fix build_flags format

--- pre_build.sh
+++ pre_build.sh
@@ -5,7 +5,7 @@ set -e
 
 # INPUT_GOARCH is a variable set by the wangyoucao577 action
 # https://github.com/wangyoucao577/go-release-action/blob/v1.40/action.yml#L109
-C_COMP="clang"
+C_COMP=""
 if [ "${INPUT_GOARCH}" = "arm64" ]; then
     C_COMP="aarch64-linux-gnu-gcc"
 elif [ "${INPUT_GOARCH}" = "amd64" ]; then
