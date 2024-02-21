d8eda44df31c028f10b2b01d77e8eba3f3ab71dc
Tarak Ben Youssef <tarak.benyoussef@dapperlabs.com>
2024-02-20 20:53:19 -0600

    export GO flags

--- pre_build.sh
+++ pre_build.sh
@@ -5,12 +5,14 @@ set -e
 
 # INPUT_GOARCH is a variable set by the wangyoucao577 action
 # https://github.com/wangyoucao577/go-release-action/blob/v1.40/action.yml#L109
+C_COMP=""
 if [ "${INPUT_GOARCH}" = "arm64" ]; then
-    CC="aarch64-linux-gnu-gcc"
+    C_COMP="aarch64-linux-gnu-gcc"
 elif [ "${INPUT_GOARCH}" = "amd64" ]; then
-    CC="x86_64-linux-gnu-gcc"
+    C_COMP="x86_64-linux-gnu-gcc"
 fi
 
-GO111MODULE=on
-CGO_ENABLED=1
-CGO_FLAGS=\""-O2 -D__BLST_PORTABLE__"\"
+export GO111MODULE=on
+export CGO_ENABLED=1
+export CC=${C_COMP}
+export CGO_FLAGS=\""-O2 -D__BLST_PORTABLE__"\"
